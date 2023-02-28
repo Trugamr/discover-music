543f7f3b5358d581e9bdd05b85c83eb813efdccc
Trugamr <trugamr@live.in>
2023-03-01 01:20:21 +0530

    ci: test deployment

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,17 @@
+import { json } from '@remix-run/cloudflare'
+import { useLoaderData } from '@remix-run/react'
+
+export function loader() {
+  return json({ now: Date.now() })
+}
+
 export default function Index() {
+  const { now } = useLoaderData<typeof loader>()
+
   return (
-    <main className="flex h-full w-full items-center justify-center bg-green-500">
+    <main className="flex h-full w-full flex-col items-center justify-center bg-green-500">
       <h1 className="text-4xl font-bold text-gray-800">discover</h1>
+      <h2>{now}</h2>
     </main>
   )
 }
